152f32bb0bf6f4acb73c6a890cc7994e7dc2299f
Katherine <kbenjumeao@eafit.edu.co>
2023-09-04 20:12:10 -0500

    Inicio interfaz visualización

--- Medetrics/visualizacion/views.py
+++ Medetrics/visualizacion/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Visualizacion
 
 # Create your views here.
+def visualizacion(request):
+    visualizacions = Visualizacion.objects.all().order_by('-date')
+    return render(request, 'visualizacion.html', {'visualizacions':visualizacions})
