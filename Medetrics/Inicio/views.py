a1d0262e79bd834fba717df22f83ad6fc76b29bd
pipesama500 <felipemartinezc004@gmail.com>
2023-09-11 12:18:32 -0500

    Fixed commit

--- Medetrics/Inicio/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def inicio(req):
-    context = {}
-    return render(req, "inicio.html",context)
--- Medetrics/reportes/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def reportes(req):
-    context = {}
-    return render(req, "reporte.html",context)
