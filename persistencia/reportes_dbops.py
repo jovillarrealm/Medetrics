3b5befeec2eaf3a045afb8c2c97661aa3d52df7e
Jorge A. Villarreal <88699002+jovillarrealm@users.noreply.github.com>
2023-10-07 12:15:28 -0500

    cleanup

--- persistencia/reportes_dbops.py
+++ persistencia/reportes_dbops.py
@@ -10,7 +10,7 @@ def send_report(
     reported = db[collection].insert_one(report).acknowledged
     return reported
 
-
+@functools.lru_cache(maxsize=1)
 def get_input_data(db: Database = get_db(), collection: str = "reportes"):
     enfermedades = ["Enfermedades"]+ db[collection].distinct("disease")
     municipio = ["Municipio"]+ db[collection].distinct("municipio")
@@ -21,32 +21,3 @@ def get_input_data(db: Database = get_db(), collection: str = "reportes"):
 
 
 
-# TODO Reemplazar con datos de DIVIPOLA
-@functools.lru_cache(maxsize=1)
-def t_input_data(db: Database = get_db(), collection: str = "mock_data"):
-    # Requires the PyMongo package.
-    # https://api.mongodb.com/python/current
-
-    places = db["places"].aggregate(
-        [
-            {
-                "$group": {
-                    "_id": "$Departamento",
-                    "municipios": {"$addToSet": "$Municipio"},
-                }
-            },
-            {"$project": {"departamento": "$_id", "municipios": 1, "_id": 0}},
-        ]
-    )
-    pack = lambda field: (field, field)
-    packlist = lambda thing: [list(map(pack, i)) for i in thing]
-    if places:
-        disease = ["Enfermedades"] + places["disease"]
-        municipios = ["Municipios"] + places["municipios"]
-        barrios = ["Barrios"] + ["medellin_barrios"]
-    else:
-        disease = ["Enfermedades"]
-        municipios = ["Municipios"]
-        barrios = ["Barrios"]
-    result = packlist((disease, municipios, barrios))
-    return result
